39862e3d1399fe3f2b300181ba116137dcfecd43
Freddy Reza <rezafreddy@gmail.com>
2022-03-05 16:00:35 -0600

    update all files to add styling and required inputs. update functionality of monthly salary.

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 $(ready);
 let totalSalary = 0;
+let dollarUS = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+});
+
 function ready() {
     $('#addButton').on('click', addEmployee);
     $(`#tableBody`).on('click', '.deleteBtn', deleteEmp);
@@ -7,47 +12,75 @@ function ready() {
 
 function addEmployee() {
     let firstName = $('#firstName').val();
-    let lastName = $('#lastName').val();
-    let idNumber = $('#idNumber').val();
-    let title = $('#title').val();
-    let salary = Number($('#salary').val());
-    let employeeObject = {
-        first: firstName,
-        last: lastName,
-        id: idNumber,
-        title: title,
-        salary: salary
+    let lastName  = $('#lastName').val();
+    let idNumber  = $('#idNumber').val();
+    let title     = $('#title').val();
+    let salary    = Number($('#salary').val());
+
+    if (firstName == '' || lastName == '' || idNumber == '' || title == '' || salary    == '') {
+        $('.required').append('* ALL FIELDS ARE REQUIRED');
+        if (firstName == '' ) {
+            $('#firstName').addClass('moreRed')
+        }
+        if (lastName == '' ) {
+            $('#lastName').addClass('moreRed')
+        }
+        if (idNumber == '' ) {
+            $('#idNumber').addClass('moreRed')
+        }
+        if (title == '' ) {
+            $('#title').addClass('moreRed')
+        }
+        if (salary == '' ) {
+            $('#salary').addClass('moreRed')
+        }
+    } else {
+        $('.required').text('');
+        $('#firstName').removeClass('moreRed')
+        $('#lastName').removeClass('moreRed')
+        $('#idNumber').removeClass('moreRed')
+        $('#title').removeClass('moreRed')
+        $('#salary').removeClass('moreRed')
+
+        let employeeObject = {
+            first: firstName,
+            last: lastName,
+            id: idNumber,
+            title: title,
+            salary: salary
+        }
+
+        $('#tableBody').append(`
+            <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${idNumber}</td>
+                <td>${title}</td>
+                <td>${dollarUS.format(salary)}</td>
+                <td><button class="deleteBtn" data-object="${employeeObject.salary}"id="deleteButton">Delete</button></td>
+            </tr>`);
+
+        totalSal(employeeObject.salary/12);
+
+        $('.input').val('');
     }
-
-    $('#tableBody').append(`
-        <tr>
-            <td>${firstName}</td>
-            <td>${lastName}</td>
-            <td>${idNumber}</td>
-            <td>${title}</td>
-            <td>${salary}</td>
-            <td><button class="deleteBtn" data-object="${employeeObject.salary}"id="deleteButton">Delete</button></td>
-        </tr>`);
-
-    totalSal(employeeObject.salary);
-
-    $('.input').val('');
 }
 
 function totalSal(sal) {
     totalSalary += sal;
+
     if (totalSalary > 20000) {
         $('#totalSalary').addClass('red');
     } else {
         $('#totalSalary').removeClass('red');
     }
-    $('#totalSalary').text(totalSalary);
+    $('#totalSalary').text(dollarUS.format(Math.abs(totalSalary)));
 }
 
 function deleteEmp() {
     let removed = $(this).data('object');
 
-    totalSal(Number(removed) * -1);
+    totalSal(removed/12 * -1);
     
     $(this).closest('tr').remove();
 }
